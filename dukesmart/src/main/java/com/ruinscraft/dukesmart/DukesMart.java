80a91fea1ad2860e699f4f658bbe671d78798e13
jianbriggs <j.ianbriggs@gmail.com>
2020-07-10 16:35:09 -0500

    Initial commit.

--- dukesmart/src/main/java/com/ruinscraft/dukesmart/DukesMart.java
+++ dukesmart/src/main/java/com/ruinscraft/dukesmart/DukesMart.java
@@ -1,6 +1,7 @@
 package com.ruinscraft.dukesmart;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -9,18 +10,22 @@ public class DukesMart extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        
+    	getLogger().info("DukesMart has been enabled!");
+    	for (Player player : Bukkit.getServer().getOnlinePlayers()) {
+    	    //playerList.put(player.getName(), playerData(player));
+    	}
     }
 
     @Override
     public void onDisable() {
-        
+    	getLogger().info("DukesMart has been disabled!");
     }
 
-    public boolean onCommand(CommandSender, sender, Command cmd, String label, String[] args){
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
         Player player = (Player) sender;
         if(cmd.getName().equalsIgnoreCase("hello")){
             player.sendMessage("Hello, world!");
+            return true;
         }
 
         return false;
